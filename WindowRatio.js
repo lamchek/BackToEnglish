96a7f971c2b4e93e935f6df14f14307016b6fdff
Aleksandr Zamiatin <aleksandr.zam1990@gmail.com>
2025-05-09 20:08:16 +0400

    193th commit

--- WindowRatio.js
+++ WindowRatio.js
@@ -2,11 +2,20 @@ function checkTabletRatio()
 {
     var availWidth = window.screen.availWidth;
     var availHeight = window.screen.availHeight;
-    console.log("WindowRatio: {0} x {1}", availWidth, availHeight);
-    var ratio = availWidth / availHeight;
+    console.log("WindowRatio: (awail) {0} x {1}", availWidth, availHeight);
+    var ratioAwail = availWidth / availHeight;
+    console.log("WindowRatio: (awail) " + ratioAwail);
+    ratioAwail = Math.round(ratioAwail * 100) / 100;
+    console.log("WindowRatio: (round): " + ratioAwail);
+
+    var width = window.screen.width;
+    var height = window.screen.height;
+    console.log("WindowRatio: {0} x {1}", width, height);
+    var ratio = width / height;
     console.log("WindowRatio: " + ratio);
     ratio = Math.round(ratio * 100) / 100;
     console.log("WindowRatio: (round): " + ratio);
+    
     if (ratio >= 0.73 && ratio <= 0.76) 
     {
         console.log("WindowRatio: looks like an Tablet");
