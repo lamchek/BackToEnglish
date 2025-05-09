056d35c5acca46cdc776a52dfb872af11bd451fa
Aleksandr Zamiatin <aleksandr.zam1990@gmail.com>
2025-05-09 19:02:02 +0400

    190th commit

---
+++ WindowRatio.js
@@ -0,0 +1,22 @@
+function checkTabletRatio() 
+{
+    var ratio = window.screen.availWidth / window.screen.availHeight;
+    console.log("WindowRatio: " + ratio);
+    ratio = Math.round(ratio * 100) / 100;
+    console.log("WindowRatio: (round): " + ratio);
+    if (ratio >= 0.73 && ratio <= 0.76) 
+    {
+        console.log("WindowRatio: looks like an Tablet");
+        return true;
+    }
+    else if (ratio >= 2.23 && ratio <= 2.43) 
+    {
+        console.log("WindowRatio: looks like an Tablet");
+        return true;
+    }
+    else 
+    {
+        console.log("WindowRatio: looks like an Phone");
+        return false;
+    }
+}
